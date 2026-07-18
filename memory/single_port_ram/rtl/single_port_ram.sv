6638c7a4aa59721997cd6aa56969555c11a9cb3b
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-07-19 01:15:41 +0600

    modified:   memory/single_port_ram/rtl/single_port_ram.sv

--- memory/single_port_ram/rtl/single_port_ram.sv
+++ memory/single_port_ram/rtl/single_port_ram.sv
@@ -1,3 +1,21 @@
-module single_port_ram(
+// ==============================================================================
+// AUTHOR:  Ahasan Ullah Khalid
+// PROJECT: sv-common-ip-library
+// MODULE:  single_port_ram
+// ASSET:   single_port_ram.sv (Synthesizable Parameterized Core)
+// ==============================================================================
+
+`include "assert_macros.svh"
+
+module single_port_ram #(
+    parameter int DATA_WIDTH = memory_pkg::RAM_DEFAULT_DATA_WIDTH,
+    parameter int ADDR_WIDTH = memory_pkg::RAM_DEFAULT_ADDR_WIDTH
+) (
+    // Interface bundles
+    clk_rst_if.slave clk_rst,
+    memory_if.slave  mem_bus
 );
+
+  localparam int DEPTH = 1 << ADDR_WIDTH;
+
 endmodule
