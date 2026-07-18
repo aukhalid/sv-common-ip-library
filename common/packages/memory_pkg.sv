08df0fd8849800645b79f6ec8ca0720240c1cc51
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-07-19 00:44:35 +0600

    modified:   common/packages/memory_pkg.sv

--- common/packages/memory_pkg.sv
+++ common/packages/memory_pkg.sv
@@ -1,2 +1,13 @@
+// ==============================================================================
+// AUTHOR: Ahasan Ullah Khalid
+// PROJECT: sv-common-ip-library
+// ASSET:   memory_pkg.sv (Global Memory Package)
+// ==============================================================================
+
 package memory_pkg;
-endpackage
+
+  //Define a parameterized memory type
+  parameter int RAM_DEFAULT_DATA_WIDTH = 32;
+  parameter int RAM_DEFAULT_ADDR_WIDTH = 32;
+
+endpackage : memory_pkg
