f03d79e1d816f38dd00b7e245f57b7c1c5bde069
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-07-20 00:03:20 +0600

    new file:   memory/single_port_ram/tb/ram_driver.sv new file:   memory/single_port_ram/tb/ram_environment.sv new file:   memory/single_port_ram/tb/ram_generator.sv new file:   memory/single_port_ram/tb/ram_monitor.sv new file:   memory/single_port_ram/tb/ram_scoreboard.sv new file:   memory/single_port_ram/tb/ram_transaction.sv new file:   memory/single_port_ram/tb/tb_ram_top.sv

---
+++ memory/single_port_ram/tb/ram_transaction.sv
@@ -0,0 +1,32 @@
+// ==============================================================================
+// AUTHOR      : Ahasan Ullah Khalid
+// PROJECT     : sv-common-ip-library
+// FILE        : ram_transaction.sv
+// ASSET       : Random Transaction Data Model Container Class
+// ==============================================================================
+
+
+class ram_transaction #(
+    int DATA_WIDTH = memory_pkg::RAM_DEFAULT_DATA_WIDTH,
+    int ADDR_WIDTH = memory_pkg::RAM_DEFAULT_ADDR_WIDTH
+);
+
+  rand logic wr_en;
+  rand logic [ADDR_WIDTH-1:0] addr;
+  rand logic [DATA_WIDTH-1:0] wr_data;
+  logic [DATA_WIDTH-1:0] rd_data;
+
+  // Constraint to ensure address is within valid range
+  constraint rw_balance_c {
+    wr_en dist {
+      1 := 50,
+      0 := 50
+    };  // 50% chance of read or write
+    addr inside {[0 : (1 << ADDR_WIDTH) - 1]};  // Address within valid range
+  }
+
+  function void display_transaction();
+    $display("Transaction: wr_en=%0b, addr=%0h, wr_data=%0h, rd_data=%0h", wr_en, addr, wr_data,
+             rd_data);
+  endfunction : display_transaction
+endclass : ram_transaction
